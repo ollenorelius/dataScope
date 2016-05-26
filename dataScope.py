669bef3bf8483d8fd1b78cd817f80b80db1a1f05
ollenorelius <norelius.olle@gmail.com>
2016-05-27 00:23:11 +0200

    added some error handling
    It will use the first serial port it can connect to now
    
    Also, limits now work

--- dataScope.py
+++ dataScope.py
@@ -1,15 +1,33 @@
 import serial
 import matplotlib.pyplot as plt
 import numpy as np
+from serial.tools import list_ports
 
-
+ 
 def plotClose(evt):
 	quit()
 
 initTimeOut = 100
-dataLength = 1000
+dataLength = 1024
 
-ser = serial.Serial('COM4', 115200)
+ports = list(list_ports.comports())
+
+if(len(ports) == 0):
+	print("No serial ports found!")
+	quit()
+
+ser = []
+for port in ports:
+	try:
+		ser = serial.Serial(port.device, 115200)
+		break
+	except serial.SerialException as e:
+		print(e)
+
+if ser == []:
+	print("Could not open any available ports. Check for other programs using serial ports.")
+	quit()
+
 
 nChannels = 1
 channelData = []
@@ -20,6 +38,7 @@ yMax = 1
 
 for i in range(initTimeOut):
 	data = ser.readline().decode().strip().split()
+
 	if data[0] == "N":
 		nChannels = len(data)-1
 		print(nChannels)
@@ -37,7 +56,7 @@ for i in range(initTimeOut):
 plt.ion()
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.set_ylim([-1, 1])
+ax.set_ylim((float(yMin), float(yMax)))
 fig.canvas.mpl_connect('close_event', plotClose)
 
 plotLines = []
@@ -53,21 +72,19 @@ ax.legend(channelNames[0])
 pointer = 0
 while True:
 	serialData = ser.readline().decode().strip().split()
-	if (serialData[0] == "D") & (len(serialData) == nChannels+2):
+	if (len(serialData) == nChannels+2) & (serialData[0] == "D"):
 		serialData = serialData[1:]
 		for iChan in range(nChannels+1):
 			channelData[pointer,iChan] = float(serialData[iChan])
 			
-
-		for iChan in range(1, nChannels+1):
-			xData = np.append(channelData[pointer+1:dataLength,0], channelData[0:pointer+1,0])
-			yData = np.append(channelData[pointer+1:dataLength,iChan], channelData[0:pointer+1,iChan])
-			plotLines[iChan-1].set_ydata(yData)
-			plotLines[iChan-1].set_xdata(xData)
-			ax.set_xlim([min(xData), max(xData)])
-
-		if pointer % 10 == 0:
-			fig.canvas.draw()
+		if pointer % 32 == 0:
+			for iChan in range(1, nChannels+1):
+				xData = np.append(channelData[pointer+1:dataLength,0], channelData[0:pointer+1,0])
+				yData = np.append(channelData[pointer+1:dataLength,iChan], channelData[0:pointer+1,iChan])
+				plotLines[iChan-1].set_ydata(yData)
+				plotLines[iChan-1].set_xdata(xData)
+				ax.set_xlim([min(xData), max(xData)])
+				fig.canvas.draw()
 
 		pointer = (pointer + 1)%dataLength
 
